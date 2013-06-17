3937b3ff8fb9061ddeb3e1420cff091f5c31068c
gcishevs <germancishevskiy@gmail.com>
2013-06-17 14:18:52 +0300

    level1

--- js/progames.js
+++ js/progames.js
@@ -1,52 +1,98 @@
-$(function()
+$(function() 
 {
-	
-	
-	 var paper = Raphael('paper', 500, 500);
-	//var circle = paper.circle(100,100, 50);
 
-	for(var i = 1; i < 5; i++)
-	 {
-	// 	var multiplier = i*5;
-	// 	paper.circle(250 + (2*multiplier), 100 + multiplier, 50 - multiplier);
+	var paper = Raphael('paper', 300, 300);
 
-		 paper.rect(10 + 75 * i, 20, 70, 40, 10)
-         .attr({gradient: '290-#526c7a-#fff', stroke: '#3b4449'})
-         .data("i", i)
-         .click(function () {
-         	this.unclick();
-            this.animate({y: 490}, 8000, function() 
-            { 
-              this.remove();  
-            });  
-         });
+	var rect;
+
+	var text;
+
+	var c_fillMatch = {	fill: "red"	};
+
+	$('#value').focus();
+
+
+	function moveAnimation() 
+	{
+		animC = Raphael.animation({	cy: 280	}, 3000, "bounce", function() 
+		{
+			this.remove();
+		});
+
+		anim = Raphael.animation({ y: 280}, 3000, "bounce", function() 
+		{
+			this.remove();
+		});
+	}
+
+	var anim = Raphael.animation(animParam, 2000);
+	var animParam = {y: 300	};
+
+	var animC = Raphael.animation(animParamC, 2000);
+	var animParamC = {cy: 300 };
+
+
+	function createSet() {
+
+		$('#value').focus();
+		var c_fill = {fill: '#2980B9',cursor: "pointer"	};
+
+		var t_fill = {fill: "white",'font-size': "15" };
+
+		var randomNum1 = getRandom(1, 9);
+
+		var randomNum2 = getRandom(1, 9)
+
+		var randomX = getRandom(30, 250);
+
+		var superValue = randomNum1 * randomNum2;
+
+		var superText = randomNum1 + " x " + randomNum2;
+
+
+		rect = paper.circle(randomX, 10, 20);
+		rect.attr(c_fill);
+		text = paper.text(randomX, 10, superText);
+		text.data('value', superValue);
+		text.attr(t_fill);
+		moveAnimation();
+		rect.animate(animC);
+		text.animateWith(rect, animParam, anim);
+
 
 	}
 
+	createSet();
+
+	setInterval(createSet, 3000);
+
+	function stop() {
+		var value = $('#value').val();
+
+		if (value == text.data("value")) {
+			text.attr(c_fillMatch);
+			rect.remove();
+			text.remove();
+			$('#value').val('');
+
+		}
 
 
-	// var rect = paper.rect(100, 200, 100, 50);
-	// var ellipse = paper.ellipse(350, 250, 100, 50);
-	// var tetronimo = paper.path("M 150 420 l 0 -50 l 50 0 l 0 50 l 50 0 l 0 50 l -150 0 l 0 -50 z");
+	}
 
-	// tetronimo.attr({
-	// 	    gradient: '90-#526c7a-#64a0c1',
-	// 	    stroke: '#3b4449',  
- //            'stroke-width': 10,  
- //            'stroke-linejoin': 'round'  
-             
-	// })
-
-	// tetronimo.animate({"transform": "r 360"}, 2000, 'bounce', function()
-	// {
-	// 	this.animate(
-	// 	{
-	// 		"transform": "r 0",
-	// 		'stroke-width': 1,
-	// 		gradient: '45-#526c7a-#64a0c1',
-	// 	}, 2000, 'bounce');
-	// });
+	$('#enter').click(function() {
+		stop();
+	});
 
 
+	$(document).keydown(function(event) {
+		if (event.keyCode == 13) {
+			stop()
+		}
+	});
+
+	function getRandom(min, max) {
+		return min + Math.floor(Math.random() * (max - min + 1));
+	}
 
 });
