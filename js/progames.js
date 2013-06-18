d9d03f5c50eb08238da268066f2ab581d404de84
gcishevs <germancishevskiy@gmail.com>
2013-06-18 10:18:02 +0300

    added more logoc for game

--- js/progames.js
+++ js/progames.js
@@ -7,19 +7,30 @@ $(function()
 
 	var text;
 
+    var interval;
+
 	var c_fillMatch = {	fill: "red"	};
+
+	var count = 0;
+
+	var time = 4000;
+
+	var countCheck = 5;
 
 	$('#value').focus();
 
+	var set = paper.set();
+
 
 	function moveAnimation() 
+
 	{
-		animC = Raphael.animation({	cy: 280	}, 3000, "bounce", function() 
+		animC = Raphael.animation({	cy: 280	}, 4000, "bounce", function() 
 		{
 			this.remove();
 		});
 
-		anim = Raphael.animation({ y: 280}, 3000, "bounce", function() 
+		anim = Raphael.animation({ y: 280}, 4000, "bounce", function() 
 		{
 			this.remove();
 		});
@@ -32,8 +43,11 @@ $(function()
 	var animParamC = {cy: 300 };
 
 
-	function createSet() {
-
+	function createSet() 
+	{
+        $('#paper').removeClass("success");
+        $('#paper').removeClass("error");
+       // $('#value').val('');
 		$('#value').focus();
 		var c_fill = {fill: '#2980B9',cursor: "pointer"	};
 
@@ -49,45 +63,75 @@ $(function()
 
 		var superText = randomNum1 + " x " + randomNum2;
 
-
 		rect = paper.circle(randomX, 10, 20);
 		rect.attr(c_fill);
 		text = paper.text(randomX, 10, superText);
 		text.data('value', superValue);
+		rect.data('value', superValue);
 		text.attr(t_fill);
+
+		set.push(rect, text);
+
+		
+
 		moveAnimation();
 		rect.animate(animC);
 		text.animateWith(rect, animParam, anim);
+
+
+		if(count >= countCheck)
+		{
+			if(time >1000)
+			{
+				time -= 500;
+			}
+			clearInterval(interval);
+        	interval = setInterval(createSet, time);
+        	countCheck += 5;
+		}	
 
 
 	}
 
 	createSet();
 
-	setInterval(createSet, 3000);
+	interval = setInterval(createSet, time);
 
 	function stop() {
 		var value = $('#value').val();
 
-		if (value == text.data("value")) {
-			text.attr(c_fillMatch);
-			rect.remove();
-			text.remove();
+set.forEach(function(elem)
+{
+	if (value == elem.data("value")) 
+	{
+			count += 0.5;
+			$('#paper').addClass("success");
+			elem.remove();
 			$('#value').val('');
-
+	}
+	else
+		{
+			$('#paper').addClass("error");
+			$('#value').val('');
 		}
+
+})
+		
 
 
 	}
 
-	$('#enter').click(function() {
+	$('#enter').click(function() 
+	{
 		stop();
 	});
 
 
 	$(document).keydown(function(event) {
 		if (event.keyCode == 13) {
-			stop()
+			stop();
+			
+
 		}
 	});
 
