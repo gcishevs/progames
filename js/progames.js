23880e0e1190e5785434667b98005ebb61be710e
gcishevs <germancishevskiy@gmail.com>
2013-06-20 10:14:42 +0300

    added background image

--- js/progames.js
+++ js/progames.js
@@ -1,5 +1,4 @@
-$(function() 
-{
+$(function() {
 
 	var paper = Raphael('paper', 300, 300);
 
@@ -7,9 +6,7 @@ $(function()
 
 	var text;
 
-    var interval;
-
-	var c_fillMatch = {	fill: "red"	};
+	var interval;
 
 	var count = 0;
 
@@ -21,108 +18,11 @@ $(function()
 
 	var set = paper.set();
 
-
-	function moveAnimation() 
-
-	{
-		animC = Raphael.animation({	cy: 280	}, 4000, "bounce", function() 
-		{
-			this.remove();
-		});
-
-		anim = Raphael.animation({ y: 280}, 4000, "bounce", function() 
-		{
-			this.remove();
-		});
-	}
-
-	var anim = Raphael.animation(animParam, 2000);
-	var animParam = {y: 300	};
-
-	var animC = Raphael.animation(animParamC, 2000);
-	var animParamC = {cy: 300 };
-
-
-	function createSet() 
-	{
-        $('#paper').removeClass("success");
-        $('#paper').removeClass("error");
-       // $('#value').val('');
-		$('#value').focus();
-		var c_fill = {fill: '#2980B9',cursor: "pointer"	};
-
-		var t_fill = {fill: "white",'font-size': "15" };
-
-		var randomNum1 = getRandom(1, 9);
-
-		var randomNum2 = getRandom(1, 9)
-
-		var randomX = getRandom(30, 250);
-
-		var superValue = randomNum1 * randomNum2;
-
-		var superText = randomNum1 + " x " + randomNum2;
-
-		rect = paper.circle(randomX, 10, 20);
-		rect.attr(c_fill);
-		text = paper.text(randomX, 10, superText);
-		text.data('value', superValue);
-		rect.data('value', superValue);
-		text.attr(t_fill);
-
-		set.push(rect, text);
-
-		
-
-		moveAnimation();
-		rect.animate(animC);
-		text.animateWith(rect, animParam, anim);
-
-
-		if(count >= countCheck)
-		{
-			if(time >1000)
-			{
-				time -= 500;
-			}
-			clearInterval(interval);
-        	interval = setInterval(createSet, time);
-        	countCheck += 5;
-		}	
-
-
-	}
-
 	createSet();
 
 	interval = setInterval(createSet, time);
 
-	function stop() {
-		var value = $('#value').val();
-
-set.forEach(function(elem)
-{
-	if (value == elem.data("value")) 
-	{
-			count += 0.5;
-			$('#paper').addClass("success");
-			elem.remove();
-			$('#value').val('');
-	}
-	else
-		{
-			$('#paper').addClass("error");
-			$('#value').val('');
-		}
-
-})
-		
-
-
-	}
-
-	$('#enter').click(function() 
-	{
+	$('#enter').click(function() {
 		stop();
 	});
 
@@ -130,11 +30,109 @@ set.forEach(function(elem)
 	$(document).keydown(function(event) {
 		if (event.keyCode == 13) {
 			stop();
-			
-
 		}
 	});
 
+
+
+	function moveAnimation()
+
+	{
+		animC = Raphael.animation({
+			cy: 280
+		}, 4000, "bounce", function() {
+			this.remove();
+		});
+
+		anim = Raphael.animation({
+			y: 280
+		}, 4000, "bounce", function() {
+			this.remove();
+		});
+	}
+
+	var anim = Raphael.animation(animParam, 2000);
+	var animParam = {
+		y: 300
+	};
+
+	var animC = Raphael.animation(animParamC, 2000);
+	var animParamC = {
+		cy: 300
+	};
+
+
+	function createSet() {
+		$('#paper').removeClass("success");
+		$('#paper').removeClass("error");
+		$('#value').focus();
+
+		var c_fill = {
+			fill: '#2980B9',
+			cursor: "pointer"
+		};
+
+		var t_fill = {
+			fill: "white",
+			'font-size': "15"
+		};
+
+		var randomNum1 = getRandom(1, 9);
+
+		var randomNum2 = getRandom(1, 9)
+
+		var randomX = getRandom(30, 250);
+
+		var answer = randomNum1 * randomNum2;
+
+		var question = randomNum1 + " x " + randomNum2;
+
+		rect = paper.circle(randomX, 10, 20);
+		rect.attr(c_fill);
+		text = paper.text(randomX, 10, question);
+		text.data('value', answer);
+		rect.data('value', answer);
+		text.attr(t_fill);
+
+		set.push(rect, text);
+
+
+		moveAnimation();
+		rect.animate(animC);
+		text.animateWith(rect, animParam, anim);
+
+
+		if (count >= countCheck) {
+			if (time > 1000) {
+				time -= 500;
+			}
+			clearInterval(interval);
+			interval = setInterval(createSet, time);
+			countCheck += 5;
+		}
+
+
+	}
+
+
+	function stop() {
+		var value = $('#value').val();
+
+		set.forEach(function(elem) {
+			if (value == elem.data("value")) {
+				count += 0.5;
+				$('#paper').addClass("success");
+				elem.remove();
+				$('#value').val('');
+			} else {
+				$('#paper').addClass("error");
+				$('#value').val('');
+			}
+
+		})
+	}
+
+	
 	function getRandom(min, max) {
 		return min + Math.floor(Math.random() * (max - min + 1));
 	}
