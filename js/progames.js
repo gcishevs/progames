458c9c21b4c9366ced3bf252854ba85ff611f00e
gcishevs <germancishevskiy@gmail.com>
2013-06-26 14:12:08 +0300

    star with xy transformation

--- js/progames.js
+++ js/progames.js
@@ -38,7 +38,10 @@ $(function() {
 	function moveAnimation()
 
 	{
-		animC = Raphael.animation({
+
+		
+
+      	animC = Raphael.animation({
 			cy: 280
 		}, 4000, "bounce", function() {
 			this.remove();
@@ -49,11 +52,28 @@ $(function() {
 		}, 4000, "bounce", function() {
 			this.remove();
 		});
+
+		animB = Raphael.animation({
+			path: _transformedPath
+		}, 4000, "bounce", function() {
+			this.remove();
+		});
+
+
 	}
+
+	var randomBX = getRandom(30, 250);
+
+	var _transformedPath = Raphael.transformPath('M14.615,4.928c0.487-0.986,1.284-0.986,1.771,0l2.249,4.554c0.486,0.986,1.775,1.923,2.864,2.081l5.024,0.73c1.089,0.158,1.335,0.916,0.547,1.684l-3.636,3.544c-0.788,0.769-1.28,2.283-1.095,3.368l0.859,5.004c0.186,1.085-0.459,1.553-1.433,1.041l-4.495-2.363c-0.974-0.512-2.567-0.512-3.541,0l-4.495,2.363c-0.974,0.512-1.618,0.044-1.432-1.041l0.858-5.004c0.186-1.085-0.307-2.6-1.094-3.368L3.93,13.977c-0.788-0.768-0.542-1.525,0.547-1.684l5.026-0.73c1.088-0.158,2.377-1.095,2.864-2.081L14.615,4.928z', 'T' + randomBX + ',260');
 
 	var anim = Raphael.animation(animParam, 2000);
 	var animParam = {
 		y: 300
+	};
+
+	var animB = Raphael.animation(animParamB, 2000);
+	var animParamB = {
+		path: _transformedPath
 	};
 
 	var animC = Raphael.animation(animParamC, 2000);
@@ -89,17 +109,29 @@ $(function() {
 
 		rect = paper.circle(randomX, 10, 20);
 		rect.attr(c_fill);
+		var ball = paper.path("M14.615,4.928c0.487-0.986,1.284-0.986,1.771,0l2.249,4.554c0.486,0.986,1.775,1.923,2.864,2.081l5.024,0.73c1.089,0.158,1.335,0.916,0.547,1.684l-3.636,3.544c-0.788,0.769-1.28,2.283-1.095,3.368l0.859,5.004c0.186,1.085-0.459,1.553-1.433,1.041l-4.495-2.363c-0.974-0.512-2.567-0.512-3.541,0l-4.495,2.363c-0.974,0.512-1.618,0.044-1.432-1.041l0.858-5.004c0.186-1.085-0.307-2.6-1.094-3.368L3.93,13.977c-0.788-0.768-0.542-1.525,0.547-1.684l5.026-0.73c1.088-0.158,2.377-1.095,2.864-2.081L14.615,4.928z");
+
+		ball.attr({  
+    		fill: '90-#526c7a-#64a0c1',
+    		'fill-opacity': 0.5,
+             stroke: 'orange'
+		});
+
+
 		text = paper.text(randomX, 10, question);
 		text.data('value', answer);
+		ball.data('value', answer);
 		rect.data('value', answer);
 		text.attr(t_fill);
+		
 
-		set.push(rect, text);
+		set.push(ball, rect, text);
 
 
 		moveAnimation();
-		rect.animate(animC);
-		text.animateWith(rect, animParam, anim);
+		ball.animate(animB);
+		text.animateWith(ball, animParam, anim);
+		rect.animateWith(text, animParamC, animC);
 
 
 		if (count >= countCheck) {
